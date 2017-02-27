5e716de7ed7f9eadbadb3c8158b275e6fa906ed9
Josh Zojonc <Josh.Zojonc@Oregonstate.edu>
2017-02-27 14:44:39 -0800

    Making orst & oregonstate use same func

--- sanity.py
+++ sanity.py
@@ -1,22 +1,10 @@
 import re
-def protect_default(l):
-  ret=[]
-  for i in l:
-    if re.search("(.*\.)?orst\.edu$",i) == None:
-      ret.append(i)
-  return ret
 
 def protect_orst(l):
-  print sys._getframe().f_code.co_name
   ret=[]
   for i in l:
-    if re.search("(.*\.)?orst\.edu$",i) == None:
+    if re.search("(.*\.)?orst|oregonstate\.edu$",i) == None:
       ret.append(i)
   return ret
 
-def protect_oregonstate(l):
-  ret=[]
-  for i in l:
-    if re.search("(.*\.)?oregonstate\.edu$",i) == None:
-      ret.append(i)
-  return ret
+
