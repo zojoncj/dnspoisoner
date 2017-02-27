25c4261e19de535bedd249bb8240a9b129de8232
Josh Zojonc <Josh.Zojonc@Oregonstate.edu>
2017-02-27 09:43:43 -0800

    move to config based whitelist

--- poisonme.py
+++ poisonme.py
@@ -17,32 +17,42 @@ def getlist(f):
     giveup("Unable to open file %s" % f)
 
 def getconfig(f):
-  print f
-  config = ConfigParser.ConfigParser()
-  config.read(f)
+  if(not os.path.isfile(f)):
+    giveup("Config file %s does not exist" % f)
+  config = ConfigParser.ConfigParser(allow_no_value=True)
+  try:
+    config.read(f)
+  except ConfigParser.ParsingError, e:
+    giveup("Error reading config file %s at %s" %(e.filename,e.errors))
+  return config
+
+def getwhitelist(list):
+  return list
+ 
+def getnames(b,w):
+  b = set(b)
+  w = set(w)
+  return b - w
+
 
 def main(arguments):
 
   parser = argparse.ArgumentParser(
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter)
-  parser.add_argument("-b", "--blacklist", help="List of domains to be poisoned." ,
-                        required=True)
   parser.add_argument("-c", "--config", help="Configuration File." ,
                         default="./config")
-  parser.add_argument("-w", "--whitelist", help="List of domains to not allow to be poisoned.", 
-                        default="./whitelist")
+  parser.add_argument("-b", "--blacklist", help="List of domains to be poisoned." ,
+                        required=True)
 
 
   args = parser.parse_args(arguments)
 
-  blacklist=getlist(args.blacklist)
-
   config=getconfig(args.config)
-
-  whitelist=getlist(args.whitelist)
+  blacklist=getlist(args.blacklist)
+  whitelist=getwhitelist(config.options('whitelist'))
+  getnames(blacklist,whitelist)
   
-
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
